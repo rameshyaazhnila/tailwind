c00b9a1d835b721daee81bad47948791d9813179
rameshyaazhnila <rameshyaazhnila@gmail.com>
2024-07-12 15:34:42 +0530

    react eith tailwind

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-white font-bold bg-slate-900 text-center">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
