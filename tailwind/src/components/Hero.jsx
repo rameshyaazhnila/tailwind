b110c24255f2143718b78f9353d3af4c61bc8578
rameshyaazhnila <rameshyaazhnila@gmail.com>
2024-07-18 21:49:52 +0530

    modify

--- tailwind/src/components/Hero.jsx
+++ tailwind/src/components/Hero.jsx
@@ -24,13 +24,13 @@ const Hero = () => {
         <h1>We Together practice code for<br></br> <span data-aos="fade-up" className='bg-gradient-to-r from-orange-300 to-orange-600 text-transparent bg-clip-text font-semibold font-extrabold'>Web Design</span></h1>
         <p className='text-xl pt-5 pb-5 z-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, nulla! Quasi sapiente facere, enim sit officiis veritatis asperiores, eveniet, numquam quia beatae reprehenderit possimus earum!</p>
         <div className='flex justify-center space-x-5 text-lg '>
-          <a href="#" className='border px-2 py-1 bg-gradient-to-r from-orange-500 to-orange-700 rounded-md'>start now</a>
-          <a href="#" className='border px-2 py-1 rounded-md'>coding sheet</a>
+          <a data-aos="zoom-in-left" href="#" className='border px-2 py-1 bg-gradient-to-r from-orange-500 to-orange-700 rounded-md'>start now</a>
+          <a  data-aos="zoom-in-right" href="#" className='border px-2 py-1 rounded-md'>coding sheet</a>
         
         </div>
         <div className='flex flex-wrap justify-center mt-7 sm:space-x-10 gap-5'>
-          <img data-aos="zoom-in-right" className='w-60 h-60 rounded-md' src={img002} alt="" />
-          <img data-aos="zoom-in-left" className='w-60 h-60 rounded-md' src={img003} alt="" />
+          <img data-aos="zoom-in-left" className='w-60 h-60 rounded-md' src={img002} alt="" />
+          <img data-aos="zoom-in-right" className='w-60 h-60 rounded-md' src={img003} alt="" />
         </div>
         
 
